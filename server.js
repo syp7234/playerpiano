907727cd0ba7a72060ca6958f54f9f0c0d54898b
steven-petrick <steven.petrick@live.com>
2021-02-16 18:43:05 -0500

    Played around and got Help button configuration set up with sample text. Kept in original style

--- server.js
+++ server.js
@@ -7,21 +7,22 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 app.use(express.static("static"));
+
+// Request route to index.html file located in the static/ directory
 router.get('/',function(req,res){
     res.sendfile(path.join(__dirname+'/static/index.html'));
-
 });
 
-//add the router
+//Add the router
 app.use('/', router);
 server.listen(3000, '127.0.0.1');
 
 console.log('Running at Port 0.0.0.0:3000');
-let rawData = fs.readFileSync('json/library.json');
+let rawData = fs.readFileSync('json/library.json'); // Library Link: Song List
 let allSongs = JSON.parse(rawData);
-console.log(allSongs);
+//console.log(allSongs); // PRINT: LIBRARY LIST
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('User connected...');
     socket.on('request library', function (data) {
         console.log(data);
         let library = JSON.parse(rawData);
